bb72dffe3934eae403f030d44f7a64249f1053fe
karan <karuaan@gmail.com>
2017-09-19 23:04:59 -0400

    All Android Projects
    Added the Android Projects

---
+++ MyForm/app/src/main/java/com/example/class2018/myform/Football_Form.java
@@ -0,0 +1,13 @@
+package com.example.class2018.myform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Football_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_football__form);
+    }
+}
